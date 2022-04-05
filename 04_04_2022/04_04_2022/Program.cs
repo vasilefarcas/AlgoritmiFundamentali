eec6c353a4026115075d2fc88cf6f91b67b62241
vasilefarcas <farcasvasile06@gmail.com>
2022-04-05 10:31:14 +0300

    Incarcare progres

---
+++ 04_04_2022/04_04_2022/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_04_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] a = new int[n, m];
+            string[] linie = Console.ReadLine().Split(' ');
+            int[] ciur = CiurEratorstene(3000);
+            int q = 0, k = 0;
+            for (int i = 0; i < linie.Length && q < n; i++)
+            {
+                if (ciur[int.Parse(linie[i])] == 0)
+                {
+                    a[q, k] = int.Parse(linie[i]);
+                    k++;
+                    if (k == m)
+                    {
+                        q++;
+                        k = 0;
+                    }
+
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        private static int[] CiurEratorstene(int n)
+        {
+            int[] ciur = new int[n];
+            for (int i = 2; i < n; i++)
+            {
+                if (ciur[i] == 0)
+                {
+                    for (int j = 2 * i; j < n; j += i)
+                    {
+                        ciur[j] = 1;
+                    }
+                }
+            }
+            return ciur;
+        }
+
+    }
+}
---
+++ PB_Info/1313/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1313
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] l = Console.ReadLine().Split(' ');
+            int m = int.Parse(l[0]), n = int.Parse(l[1]);
+            int[,] a = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                string[] linie = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                    a[i, j] = int.Parse(linie[j]);
+            }
+            int p = int.Parse(Console.ReadLine());
+            int[,] b = new int[n, p];
+            for (int i = 0; i < n; i++)
+            {
+                string[] linie = Console.ReadLine().Split(' ');
+                for (int j = 0; j < p; j++)
+                    b[i, j] = int.Parse(linie[j]);
+            }
+            int[,] c = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        c[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(c[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
