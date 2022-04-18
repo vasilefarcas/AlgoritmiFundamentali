23ac9f5d2281fc50e98a1733c66bf6551746c55a
vasilefarcas <farcasvasile06@gmail.com>
2022-04-18 11:41:01 +0300

    18_04_2022

---
+++ 18_04_2022/18_04_2022/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_04_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            while (n % 2 != 1)
+            {
+                Console.WriteLine("Introduceti o valoare impara:");
+                n = int.Parse(Console.ReadLine());
+            }
+            int n2 = n / 2;
+            int[,] a = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        a[i, j] = 0;
+                    else if (i + j == n - 1)
+                        a[i, j] = 0;
+                    else if (n2 == j)
+                        a[i, j] = 0;
+                    else if (n2 == i)
+                        a[i, j] = 0;
+                    else if (i < j && i + j < n - 1 && i < n2 && j < n2)
+                        a[i, j] = 1;
+                    else if (i < j && i + j < n - 1 && i < n2 && j > n2)
+                        a[i, j] = 2;
+                    else if (i < j && i + j > n - 1 && i < n2 && j > n2)
+                        a[i, j] = 3;
+                    else if (i < j && i + j > n - 1 && i > n2 && j > n2)
+                        a[i, j] = 4;
+                    else if (i > j && i + j > n - 1 && i > n2 && j > n2)
+                        a[i, j] = 5;
+                    else if (i > j && i + j > n - 1 && i > n2 && j < n2)
+                        a[i, j] = 6;
+                    else if (i > j && i + j < n - 1 && i > n2 && j < n2)
+                        a[i, j] = 7;
+                    else if (i > j && i + j < n - 1 && i < n2 && j < n2)
+                        a[i, j] = 8;
+                }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 18_04_2022/Zone spirala/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zone_spirala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 14;
+            int[,] a = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = 1;
+                }
+            for (int i = 0; i < n; i++)
+                a[0, i] = 0;
+            for (int i = 0; i < n; i++)
+                a[i, n - 1] = 0;
+            for (int i = n - 1; i > 0; i--)
+                a[n - 1, i] = 0;
+            for (int i = n - 1; i > 0; i--)
+                a[i, 0] = 0;
+            Afisare(a, n);
+
+            int count = 0;
+            for (int i = 1; i < n - 1; i++)
+                a[0, i] = ++count;
+            count--;
+            for (int i = 0; i < n - 1; i++)
+                a[i, n - 2] = ++count;
+            count--;
+            for (int i = n - 1; i > 1; i--)
+                a[n - 2, i - 1] = ++count;
+            count--;
+            for (int i = n - 2; i > 0; i--)
+                a[i, 1] = ++count;
+            Afisare(a, n);
+            for (int i = 1; i <= n-3; i++)
+            {
+                for (int j = 2; j <= n-3; j++)
+                {
+                    a[i, j] = 0;
+                }
+            }
+            Afisare(a, n);
+            Console.ReadKey();
+        }
+        static void Afisare(int[,] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
