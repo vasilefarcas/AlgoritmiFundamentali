08895f40fea4c0b6109e2ff04f79281f864c0df7
vasilefarcas <farcasvasile06@gmail.com>
2022-05-10 12:47:49 +0300

    09_05_2022

---
+++ 09_05_2022/09_05_2022/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _09_05_2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        Pen blackPen = new Pen(Color.Black);
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            StreamReader read = new StreamReader(@"..\..\..\TextFile1.txt");
+            List<Point> points = new List<Point>();
+            string linie;
+            int count=0;
+            do
+            {
+                linie = read.ReadLine();
+                points.Add(CitirePunct(read.ReadLine()));
+                g.DrawEllipse(blackPen, ReturnareX(points[count]), ReturnareY(points[count]),1,1);
+                count++;
+            }
+            while (linie != null);
+
+
+        }
+        Point CitirePunct(string a)
+        {
+            int x, y;
+            string[] buff = a.Split(' ');
+            x = int.Parse(buff[0]);
+            y = int.Parse(buff[1]);
+            return new Point(x, y);
+        }
+        int ReturnareX(Point x) => x.X;
+        int ReturnareY(Point x) => x.Y;
+
+    }
+}
---
+++ Interviu/Interviu/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interviu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fibo(300);
+            // Console.WriteLine(Palindrom("abcbbba"));
+            Console.WriteLine(Eliminare("mere",'e'));
+            Console.ReadKey();
+        }
+        public static void Fibo(int n)
+        {
+            int a = 1, b = 0, c = 0;
+            while (a <= n)
+            {
+                Console.WriteLine(c + " ");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+        public static bool Palindrom(string sir)
+        {
+            int lungime = sir.Length - 1, i = 0;
+            while (sir[i] == sir[lungime] && i != lungime && i<lungime)
+            {
+                i++;
+                lungime--;
+            }
+            if (i == lungime || i > lungime)
+                return true;
+            return false;
+        }
+        public static string Oglindit(string n)
+        {
+            string ogl = string.Empty;
+
+            return ogl;
+        }
+
+        public static string Eliminare(string s,char a)
+        {
+            string sir = string.Empty;
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] != a)
+                    sir += s[i];
+            }
+            return sir;
+        }
+    }
+}
