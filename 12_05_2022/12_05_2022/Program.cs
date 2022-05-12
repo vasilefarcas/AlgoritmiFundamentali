25d498097c8f1dbcf83759f20885ab0ae4b29054
vasilefarcas <farcasvasile06@gmail.com>
2022-05-12 10:09:54 +0300

    12_05_2022
    Citire matrice din fisier, afisare matrice, parcurgere in adancime, determinare drum din 0,0 in n-1,n-1

---
+++ 12_05_2022/12_05_2022/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_05_2022
+{
+    internal class Program
+    {
+        static Random r = new Random();
+        static int n, m;
+        static int[,] a;
+        static bool[,] b;
+        static bool ok = false;
+        static void Main(string[] args)
+        {
+            // Se da o matrice de val 0 si 1 de dim (n,m).
+            // Sa se afiseze daca exista drum de 0 de la locatia (0,0) la (n-1,n-1)
+            n = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            a = new int[n, m];
+            b = new bool[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = r.Next(2);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            PA(0, 0);
+            if (ok == true)
+                Console.WriteLine("Da");
+            else
+                Console.WriteLine("Nu");
+            Console.ReadKey();
+        }
+        public static void PA(int i, int j)
+        {
+            if (i >= 0 && j >= 0 && i < n && j < m && !b[i, j] && a[i, j] == 0)
+            {
+                if (i == n - 1 && j == m - 1)
+                    ok = true;
+                b[i, j] = true;
+                PA(i - 1, j);
+                PA(i, j + 1);
+                PA(i + 1, j);
+                PA(i, j - 1);
+            }
+        }
+    }
+}
---
+++ 12_05_2022/ProblemaPlatouri/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ProblemaPlatouri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = readM(@"D:\Drive\OneDrive\Facultate\Semestrul II\AlgoritmiFundamentali\12_05_2022\ProblemaPlatouri\bin\TextFile1.txt");
+            viewM(a);
+            Console.ReadKey();
+        }
+        static int[,] readM(string filename)
+        {
+            int[,] toR;
+            string buffer;
+            List<string> T = new List<string>();
+            TextReader load = new StreamReader(filename);
+            while ((buffer = load.ReadLine()) != null)
+                T.Add(buffer);
+            load.Close();
+            int n = T.Count;
+            int m = T[0].Split(' ').Length;
+            toR = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                string[] ld = T[i].Split(' ');
+                for (int j = 0; j < ld.Length; j++)
+                    toR[i, j] = int.Parse(ld[j]);
+
+            }
+            return toR;
+        }
+        static void viewM(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
